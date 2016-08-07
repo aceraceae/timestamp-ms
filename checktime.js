34b44a0ad783601375879eac4ce2ad9e33453503
aceraceae <yackoo@ymail.com>
2016-08-07 04:29:38 +0200

    added checktime functionality

--- checktime.js
+++ checktime.js
@@ -1,11 +1,44 @@
 var moment = require('moment');
 
 module.exports = function(time) {
-    var date = new Date(Number(time));
-    var date2 = moment(date.toString(), "MMMM-DD-YYYY");
-    console.log(date);
-        //return { unix: null, normal: null};
-        console.log(date2);
-        return { unix: time, normal: date2};
+    var testInput = (/w/i).test(time);
+
+
+    var timeArr = time.split(/[\W\_]/).filter(i => i !== "");
+    var dateToConcat = {};
+
+    dateToConcat.day = timeArr.filter((e, idx) => {
+        return (/\d/).test(e) && e.length < 3 && idx !== 1;
+    })[0];
+    dateToConcat.month = timeArr.filter((e, idx) => {
+        return (/\D/).test(e) || Number(e) < 13 && idx == 1;
+    })[0];
+    dateToConcat.year = timeArr.filter(e => {
+        return (/\d/).test(e) && e.length > 2;
+    })[0];
+
+
+    var natural = moment(
+        `${dateToConcat.day}-${dateToConcat.month}-${dateToConcat.year}`,
+        ['DD-MM-YYYY', 'DD-MMM-YYYY', 'DD-MMMM-YYYY'])
+        .format("DD MMMM YYYY");
+    var unix = new Date(natural).getTime();
+    //var natural = null;
+     /*
+    if(testInput) {
+        natural = moment(time).format("MMMM-DD-YYYY");
+        unix = new Date(natural).getTime();
+        var unixTime = Number(unix)/1000;
+        console.log(unixTime);
+    } else {
+        unix =
+        natural = moment(new Date(time*1000)).format("MMMM-DD-YYYY");
+    }
+     */
+    //var date2 = moment(date.toString(), "MMMM-DD-YYYY");
+    console.log(timeArr);
+    console.log(dateToConcat);
+    //console.log(date2);
+    return { unix, natural };
 
 };
--- server.js
+++ server.js
@@ -15,10 +15,8 @@ app.get('/', function (req, res) {
 
 app.get('/:time', function (req, res) {
 	var time = req.params.time;
-	console.log(time);
-	//resp = checkTime(time);
-
-	res.send(`Merso`);
+	//console.log(time);
+	res.send(checkTime(time));
 
 });
 
@@ -26,3 +24,16 @@ var port = process.env.PORT || 8080;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
+
+
+/*
+User Story: I can pass a string as a parameter, and it will check to see whether that string contains either a unix timestamp or a natural language date (example: January 1, 2016).
+
+User Story: If it does, it returns both the Unix timestamp and the natural language form of that date.
+
+User Story: If it does not contain a date or Unix timestamp, it returns null for those properties.
+
+Once you've finished implementing these user stories, click the "I've completed this challenge" button and enter the URLs for both your GitHub repository and your live app running on Heroku.
+
+You can get feedback on your project by sharing it with your friends on Facebook.
+*/
